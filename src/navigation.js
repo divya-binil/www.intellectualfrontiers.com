e132d107e0f51aa67972e79a8a6be403e4918d52
Sreejith K <sreejith.kalarikkal@citrusinformatics.com>
2023-12-12 12:52:47 +0530

    chore: add new pages to menu

--- src/navigation.js
+++ src/navigation.js
@@ -22,14 +22,14 @@ export const headerData = {
       //     href: getPermalink('/homes/personal'),
       //   },
       // ],
-      href: '#',
+      href: '/',
     },
     {
       text: 'About Us',
-      href: '#',
+      href: '/about',
     },
     {
-      text: 'Patient Assets',
+      text: 'Patent Assets',
       //   {
       //     text: 'Lead Generation',
       //     href: getPermalink('/landing/lead-generation'),
@@ -55,22 +55,22 @@ export const headerData = {
       //     href: getPermalink('/landing/subscription'),
       //   },
       // ],
-      href: '#',
+      href: getPermalink('/patentassets'),
     },
     {
       text: 'FAQs',
-      href: '#',
+      href: '/faq',
     },
     {
       text: 'Download Center',
-      href: '#',
+      href: 'click-through',
     },
     {
       text: 'Resources',
       href: '/patent',
     },
   ],
-  actions: [{ text: 'Contact/Chatbot', href: '/', target: '_blank' }],
+  actions: [{ text: 'Contact/Chatbot', href: 'contact', target: '_blank' }],
 };
 
 export const footerData = {
@@ -103,8 +103,8 @@ export const footerData = {
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: '#' },
+   
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
